41c90802c8405e7ab8ce4d16662260ee1c058804
Stas Syrota <mustas@pm.me>
2024-01-05 20:24:37 +0100

    clean a bit

--- hotdog/src/model/label.rs
+++
@@ -1,2 +0,0 @@
-// Generated labels from labels.txt
-include!("/home/sm/Dropbox/DTU/rtc/rtmls/hotdog/squeezenet/label.rs");
--- hotdog/src/model/squeezenet.rs
+++
@@ -1,6 +0,0 @@
-//  Generated model from squeezenet1.onnx
-mod internal_model {
-    include!("/home/sm/Dropbox/DTU/rtc/rtmls/hotdog/squeezenet/squeezenet1.rs");
-}
-
-pub use internal_model::*;
